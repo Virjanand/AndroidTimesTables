970404a0bd7b32e753843b28e30e72791f058301
Virjanand <virjanand@virjanand.com>
2019-01-26 16:52:59 +0100

    Add seekBar for numbers and display in Log

--- app/src/main/java/com/virjanand/timestables/MainActivity.java
+++ app/src/main/java/com/virjanand/timestables/MainActivity.java
@@ -2,6 +2,8 @@ package com.virjanand.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar numberSeekBar = findViewById(R.id.numberSeekBar);
+        numberSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Number selected: ", Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
