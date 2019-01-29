34ca02098f8b40aaa7010755735b74379ce1d3ae
Virjanand <virjanand@virjanand.com>
2019-01-29 06:20:06 +0100

    Changed discrete seek bar to normal seek bar and this solved the problems of a non moving seekbar

--- app/src/main/java/com/virjanand/timestables/MainActivity.java
+++ app/src/main/java/com/virjanand/timestables/MainActivity.java
@@ -3,20 +3,40 @@ package com.virjanand.timestables;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SeekBar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView timesTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar numberSeekBar = findViewById(R.id.numberSeekBar);
+        final SeekBar numberSeekBar = findViewById(R.id.numberSeekBar);
+        timesTable = findViewById(R.id.timesListView);
+
+        numberSeekBar.setMax(20);
+        numberSeekBar.setProgress(10);
+
         numberSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("Number selected: ", Integer.toString(progress));
+                int min = 1;
+                int timesTableNumber;
+
+                if (progress < min) {
+                    timesTableNumber = min;
+                    numberSeekBar.setProgress(min);
+                } else {
+                    timesTableNumber = progress;
+                }
+                fillTimesList(timesTableNumber);
             }
 
             @Override
@@ -29,5 +49,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void fillTimesList(int progress) {
+
+        ArrayList<String> timesList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            timesList.add(Integer.toString(i * progress));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, timesList);
+
+        timesTable.setAdapter(adapter);
     }
 }
